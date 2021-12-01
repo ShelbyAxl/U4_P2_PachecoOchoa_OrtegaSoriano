c275df8609a9fa15353298681f2be0f87d83725d
luisa <luisa@DESKTOP-OGFB2H9>
2021-11-30 21:58:54 -0700

    Information

---
+++ src/ConexionDB.java
@@ -0,0 +1,32 @@
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author luisa
+ */
+public class ConexionDB {
+    
+    ConnectionString connectionString = new ConnectionString("mongodb+srv://PussyDestroyerAdmin:ShelbyAxl@cluster0.iqf9p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+    MongoClientSettings settings = MongoClientSettings.builder()
+            .applyConnectionString(connectionString)
+            .build();
+    MongoClient mongoClient = MongoClients.create(settings);
+    MongoDatabase database = mongoClient.getDatabase("ejemplo1");
+
+    
+    public MongoDatabase obtenerDB(){
+        return database;
+    }
+    
+}
